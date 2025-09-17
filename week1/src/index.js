839b02e589f2a87625cbcab9d2066d7c5c2236a6
minhyeok-1 <mhyeokhongmin@naver.com>
2025-09-17 10:02:13 +0000

    week1

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,10 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/',(req,res)=> { 
+    res.send('<h1>안녕!</h1>');
+ });
+app.get('/hi',(req,res)=> { res.send('<h1>hello!</h1>'); });
+
+app.listen(3000, () => {console.log('Server listening on port 3000!')});
